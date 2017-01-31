e458aaac2b003a5c3c0241f164756d12326841e1
trinoy hazarika <trinoy hazarika>
2017-01-30 23:50:41 -0500

    added mongo link

--- bgw/model/models.server.js
+++ bgw/model/models.server.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
 
-    var connectionString = 'mongodb://localhost:27017/test';
-    //var connectionString = 'mongodb://webdev1:ABcd1234@ds033126.mlab.com:33126/mongo_th';
+    //var connectionString = 'mongodb://localhost:27017/test';
+    var connectionString = 'mongodb://root:password@ds137749.mlab.com:37749/bgw';
 
     if(process.env.HEROKU_MONGODB_DB_PASSWORD) {
         connectionString = process.env.HEROKU_MONGODB_URL_PREFIX +
